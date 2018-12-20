ad99aebb8933527966bc848712b4706fac193bf8
0verseerPepek <44055631+0verseerPepek@users.noreply.github.com>
2018-12-20 21:15:22 +0200

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,6 +13,7 @@ Devise.setup do |config|
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
+  config.secret_key = '-- secret key --'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
